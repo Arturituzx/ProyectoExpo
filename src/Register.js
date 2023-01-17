5ad3891f7c3da85d7435c8e9be9753f17ef44366
JoseMiguel1610 <chela_16_10@hotmail.com>
2023-01-17 10:59:20 -0500

    changes datetime

--- src/Register.js
+++ src/Register.js
@@ -1,101 +1,170 @@
 import { useNavigation } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, Dimensions,TouchableHighlight } from 'react-native';
-import Svg, { Path, Ellipse} from "react-native-svg";
-const {width, height} = Dimensions.get('window');
+import { Input } from 'native-base'
+import { StyleSheet, Text, View, TextInput, Dimensions, TouchableHighlight } from 'react-native';
+import DateTimePicker from "@react-native-community/datetimepicker"
+import Icon from 'react-native-vector-icons/Ionicons';
+import Icon2 from 'react-native-vector-icons/AntDesign';
+import Icon3 from 'react-native-vector-icons/Entypo';
+import Icon4 from 'react-native-vector-icons/FontAwesome';
+import Icon5 from 'react-native-vector-icons/Feather';
+import * as Icons from "react-native-heroicons/solid";
+import Svg, { Path, Ellipse } from "react-native-svg";
+import { useState } from 'react';
+const { width, height } = Dimensions.get('window');
 
 export default function Register() {
-    function SvgTop(){
-      return(
-        <Svg
-      width={428}
-      height={320}
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <Ellipse
-        cx={327}
-        cy={238}
-        rx={113}
-        ry={82}
-        fill="#BFE3B2"
-        fillOpacity={0.38}
-      />
-      <Path
-        d="M0 0h428v229.78s-126-19.766-159 11.53-85.5 50.238-155 43.238-114-9.471-114-9.471V0Z"
-        fill="#C1BFFF"
-      />
-    </Svg>
-      )
-    }
-    return(
-        <View style={styles.mainContainer}>
-        <View style={styles.containerSvg}>
-        <SvgTop/>
+  const [date, setDate] = useState(new Date())
+  const [mode, setMode] = useState('date');
+  const [text, setText] = useState();
+  const [show, setShow] = useState(false);
+
+  const showMode = (currentMode) => {
+    setShow(true)
+    setMode(currentMode)
+  };
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
+    setShow(Platform.OS === 'ios')
+    let tempDate = new Date(currentDate);
+    setDate(tempDate)
+    setText(joinDate(tempDate))
+  }
+
+  function joinDate(newDate) {
+    let y = newDate.getFullYear();
+    let m = newDate.getMonth() + 1;
+    let d = newDate.getDate();
+    m = (m < 10) ? '0' + m : m;
+    d = (d < 10) ? '0' + d : d;
+    return [d, m, y].join('-')
+  }
+
+  function SvgTop() {
+    return (
+      <Svg
+        width={428}
+        height={320}
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <Ellipse
+          cx={327}
+          cy={238}
+          rx={113}
+          ry={82}
+          fill="#BFE3B2"
+          fillOpacity={0.38}
+        />
+        <Path
+          d="M0 0h428v229.78s-126-19.766-159 11.53-85.5 50.238-155 43.238-114-9.471-114-9.471V0Z"
+          fill="#C1BFFF"
+        />
+      </Svg>
+    )
+  }
+  return (
+    <View style={styles.mainContainer}>
+      {show && <DateTimePicker testID='dateTimePicker' value={date} mode={mode} display='default' onChange={onChange} />}
+      <View style={styles.containerSvg}>
+        <SvgTop />
+      </View>
+      <View style={styles.container}>
+        <Text style={styles.title}>Formulario de Registro</Text>
+        <TextInput style={styles.Textinput}
+          placeholder='Nombres'
+        />
+        <TextInput style={styles.Textinput}
+          placeholder='Apellido Paterno' />
+        <View style={styles.searchSection}>
+
+          <TextInput 
+            style={styles.input}
+            editable={false}
+            placeholder="Fecha de Nacimiento"
+            value={text}
+            onChangeText={(searchString) => { this.setState({ searchString }) }}
+            underlineColorAndroid="transparent"
+          />
+          <Icons.CalendarDaysIcon onPress={() => showMode('date')}
+            color='#0096D6'
+            style={{ marginRight: 6 }}
+          />
         </View>
-            <View style={styles.container}>
-                <Text style={styles.title}>Formulario de Registro</Text>
-                    <TextInput style={styles.Textinput}
-                    placeholder='Nombres'
-                    />
-                    <TextInput style={styles.Textinput}
-                    placeholder='Apellido Paterno'/>
-                    <TextInput style={styles.Textinput}
-                    placeholder='Correo'/>
-                    <TextInput style={styles.Textinput}
-                    secureTextEntry={true}
-                    placeholder='Contraseña'/>
-                    <TextInput style={styles.Textinput}
-                    placeholder='Celular'/>
-                    <TouchableHighlight style={styles.button}>
-                        <Text style={styles.textButton}>Send</Text>
-                    </TouchableHighlight>
-            </View>
-        </View>
-    );
+        {/* <TextInput style={styles.Textinput} editable={false} color="black" keyboardType='numeric' textAlign="center" border="1" borderColor='cofco.50' placeholder={text} /> */}
+        <TextInput style={styles.Textinput}
+          placeholder='Correo' />
+        <TextInput style={styles.Textinput}
+          secureTextEntry={true}
+          placeholder='Contraseña' />
+        <TextInput style={styles.Textinput}
+          placeholder='Celular' />
+        <TouchableHighlight style={styles.button}>
+          <Text style={styles.textButton}>Send</Text>
+        </TouchableHighlight>
+      </View>
+    </View>
+  );
 }
 
 
 const styles = StyleSheet.create({
-    mainContainer:{
-      backgroundColor: "#f1f1f1",
-      flex: 1,
-    },
-    container:{
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    Textinput:{
-        padding: 10,
-        paddingStart: 20,
-        width: '80%',
-        height: 50,
-        marginTop: 20,
-        borderRadius: 30,
-        backgroundColor: '#fff'
-        },
-    title:{
-        textAlign: 'center',
-        fontSize: 18,
-        marginBottom: 15
-    },
-    button:{
-        width: '20%',
-        backgroundColor: 'skyblue',
-        marginTop: 20,
-        paddingTop: 15,
-        paddingBottom: 15,
-        borderRadius: 25
-    },
-    textButton:{
-        textAlign:'center',
-        color: 'white'
-    },
-    containerSvg:{
-        width: width,
-        justifyContent: 'flex-start',
-        alignItems: 'center',
-    },
+  mainContainer: {
+    backgroundColor: "#f1f1f1",
+    flex: 1,
+  },
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  Textinput: {
+    padding: 10,
+    paddingStart: 20,
+    width: '80%',
+    height: 50,
+    marginTop: 20,
+    borderRadius: 30,
+    backgroundColor: '#fff'
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 18,
+    marginBottom: 15
+  },
+  button: {
+    width: '20%',
+    backgroundColor: 'skyblue',
+    marginTop: 20,
+    paddingTop: 15,
+    paddingBottom: 15,
+    borderRadius: 25
+  },
+  textButton: {
+    textAlign: 'center',
+    color: 'white'
+  },
+  containerSvg: {
+    width: width,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  searchSection: {
+    flexDirection: 'row',
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginTop: 20,
+    width: '80%',
+    backgroundColor: '#fff',
+    borderRadius: 30,
+    padding: 10,
+    paddingStart: 20,
+  },
+  searchIcon: {
+  },
+  input: {
+    color: "#000"
+  },
 
 
 });
